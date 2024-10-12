9b2e57f9e220cb5b73d59fe52fa582901077ef17
Carlos Castillo <castillo.st@gmail.com>
2024-10-12 19:04:14 +0200

    Add eslint config for tests within functions

--- functions/.eslintrc.cjs
+++ functions/.eslintrc.cjs
@@ -5,13 +5,21 @@ module.exports = {
   },
   extends: 'standard-with-typescript',
   overrides: [
+    {
+      'files': ['**/*.spec.ts', '**/*.test.ts'],
+      'parserOptions': {
+        'project': './tsconfig.tests.json'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: ['./tsconfig.json', './tsconfig.tests.json']
   },
   ignorePatterns: ['node_modules'],
   rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'n/handle-callback-err': 'off',
   }
 }
