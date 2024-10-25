fd20ea6d007d0ebc7f17c3b838250f0863f4b01b
Carlos Castillo <castillo.st@gmail.com>
2024-10-25 20:39:18 +0200

    Showcase an example with function split in several files

--- functions/src/update-counter/update-counter.ts
+++ functions/src/update-counter/update-counter.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
+import { Counter } from './counter'
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   if (event.httpMethod !== 'GET') {
@@ -16,7 +17,7 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
   }
   const { queryStringParameters:{ counterValue, action } } = event
 
-  if (counterValue == null || action == null) {
+  if (counterValue == null || action == null || isNotANumber(counterValue)) {
     return {
       statusCode: 400,
       headers: { 'Content-Type': 'application/json' },
@@ -24,19 +25,19 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     }
   }
 
-  const counter = parseInt(counterValue, 10)
+  const counter = new Counter(parseInt(counterValue, 10))
   switch (action) {
     case 'increase':
       return {
         statusCode: 200,
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ counter: counter + 1 })
+        body: JSON.stringify({ counter: counter.increment().getCount() })
       }
     case 'decrease':
       return {
         statusCode: 200,
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ counter: counter - 1 })
+        body: JSON.stringify({ counter: counter.decrement().getCount() })
       }
     default:
       return {
@@ -45,6 +46,10 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
         body: JSON.stringify({ message: 'action must be "increase" or "decrease"' })
       }
   }
+}
+
+function isNotANumber(counterValue: unknown): boolean {
+  return isNaN(Number(counterValue));
 }
 
 export { handler }
